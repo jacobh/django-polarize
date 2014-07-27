08a65747d608fcc530adf6291a95104d4348eae6
Jacob Haslehurst <jacob@haslehurst.net>
2014-07-28 02:11:05 +1000

    apply RatingTargetMixin to test model

--- tests/models.py
+++ tests/models.py
@@ -1,7 +1,9 @@
 from __future__ import unicode_literals
 from django.db import models
 
+from generic_ratings.model_mixins import RatingTargetMixin
 
-class TextSnippet(models.Model):
+
+class TextSnippet(RatingTargetMixin, models.Model):
     text = models.TextField()
     user = models.ForeignKey('auth.User')
