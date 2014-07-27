b1319171bf73531ed5eee532362852200bf714f6
Jacob Haslehurst <jacob@haslehurst.net>
2014-07-28 02:32:58 +1000

    Add setup.py, rename to django-polarize

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-polarize',
+    version='0.1.0',
+    description='Generic ratings app for Django',
+    author='Jacob Haslehurst',
+    author_email='jacob@haslehurst.net',
+    url='https://github.com/hzy/django-generic-ratings',
+    packages=find_packages(exclude=['tests']),
+    install_requires=['django', 'south']
+)
